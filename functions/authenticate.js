3ea4638f43a04d288b29b8962013766672733df5
Sri Kakarla <31450756+charanksri@users.noreply.github.com>
2024-03-25 19:43:12 -0500

    Update authenticate.js

--- functions/authenticate.js
+++ functions/authenticate.js
@@ -1,17 +1,20 @@
+// authenticate.js
+
 const fetch = require("node-fetch");
-const btoa = require("btoa");
 
-const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+// Function to authenticate and get access token
+async function authenticate() {
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
-exports.handler = async (event, context) => {
   try {
     const response = await fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization:
-          "Basic " + btoa(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET),
+          "Basic " +
+          Buffer.from(clientId + ":" + clientSecret).toString("base64"),
       },
       body: "grant_type=client_credentials",
     });
@@ -21,15 +24,24 @@ exports.handler = async (event, context) => {
     }
 
     const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ access_token: data.access_token }),
-    };
+    return data.access_token;
   } catch (error) {
     console.error("Authentication error:", error);
+    throw error; // Rethrow error to handle it outside
+  }
+}
+
+exports.handler = async (event, context) => {
+  try {
+    const accessToken = await authenticate();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ accessToken }),
+    };
+  } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Authentication failed" }),
+      body: JSON.stringify({ error: "Internal Server Error" }),
     };
   }
 };
