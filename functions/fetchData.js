f4f7ff1012fcc6bb191f35fc180e01b2dbc9601c
Sri Kakarla <31450756+charanksri@users.noreply.github.com>
2024-03-27 19:36:13 -0500

    Add files via upload

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -1,11 +1,13 @@
-const axios = require("axios");
+// fetchData.js
+
+import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
   try {
-    const response = await axios.get(
+    const response = await fetch(
       "https://raw.githubusercontent.com/charanksri/raagalahari/ae84b9ee1fab94eff38bf2670a9b17ceff35ef45/data.json"
     );
-    const data = response.data;
+    const data = await response.json();
     return {
       statusCode: 200,
       body: JSON.stringify(data),
